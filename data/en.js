0d02e5e8c6bf735da82daf9f52e67f1b558e6cd1
ValkiriA8735 <ValeriArteeva87@gmail.com>
2023-12-19 16:16:49 +0300

    first commit

---
+++ data/en.js
@@ -0,0 +1,12 @@
+export default {
+    translation: {
+      title: "Registration",
+      text: "Enter login",
+      changeLang: "Русский",
+      mail: "Enter email",
+      password: "Enter password",
+      confirm_password: "Confirm password",
+      reg: "register",
+    },
+  };
+  
---
+++ data/ru.js
@@ -0,0 +1,11 @@
+export default {
+    translation: {
+      title: "Registration",
+      text: "Enter login",
+      changeLang: "Русский",
+      mail: "Enter email",
+      password: "Enter password",
+      confirm_password: "Confirm password",
+      reg: "register",
+    },
+};
